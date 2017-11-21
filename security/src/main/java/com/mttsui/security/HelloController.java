8d284014277124bb27d38d0df59a050a869e6c8a
Sam Tsui <xzs603@foxmail.com>
2017-11-21 23:29:08 +0800

    加入springsecurity

---
+++ security/src/main/java/com/mttsui/security/HelloController.java
@@ -0,0 +1,23 @@
+package com.mttsui.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/11/21 0021.
+ */
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
---
+++ security/src/main/java/com/mttsui/security/SecurityApplication.java
@@ -0,0 +1,14 @@
+package com.mttsui.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2017/11/21 0021.
+ */
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
