daae6858ba7941da2891e65d6f837cadf6d97ee3
Sam Tsui <xzs603@foxmail.com>
2017-12-01 22:49:47 +0800

    Merge branch 'master' of https://github.com/xzs603/springboot-demos
    # Conflicts:
    #	security/src/main/java/com/mttsui/security/controller/CrudController.java
    #	security/src/main/resources/mybatis/mybatis-generator.xml

--- security/src/main/java/com/mttsui/security/controller/UserController.java
+++ security/src/main/java/com/mttsui/security/controller/UserController.java
@@ -2,14 +2,15 @@ package com.mttsui.security.controller;
 
 import com.mttsui.security.model.entity.SysUser;
 import com.mttsui.security.model.mapper.SysUserMapper;
+import com.mttsui.security.util.CommonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
@@ -24,28 +25,33 @@ public class UserController {
         return sysUserMapper.list();
     }
 
-    @RequestMapping("/add")
+    @RequestMapping("/create")
     @ResponseBody
-    public SysUser add(SysUser sysUser) {
-        System.out.println(sysUser);
+    public SysUser create(SysUser sysUser) {
+        sysUser.setId(CommonUtil.genUUID());
+        sysUserMapper.insert(sysUser);
         return sysUser;
     }
 
     @RequestMapping("/{id}/update")
     @ResponseBody
     public SysUser update(SysUser sysUser) {
-            System.out.println(sysUser);
+        sysUserMapper.updateByPrimaryKey(sysUser);
         return sysUser;
     }
 
     @RequestMapping("/{id}/delete")
     @ResponseBody
-    public SysUser delete(SysUser sysUser) {
-        return sysUser;
+    public Map<String, Object> delete(String id) {
+        Map<String, Object> map = new HashMap<>();
+        sysUserMapper.deleteByPrimaryKey(id);
+        map.put("success", true);
+        return map;
     }
 
     @RequestMapping("/crud")
     public String crud(SysUser sysUser) {
         return "/user/crud";
     }
+
 }
