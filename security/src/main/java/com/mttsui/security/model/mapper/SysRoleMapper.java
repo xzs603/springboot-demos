ac1aa2924057b4752ff2e7f0ccc58d433a477c98
mttsui <xzs603@gmail.com>
2017-12-01 19:34:29 +0800

    密码加入md5校验

--- security/src/main/java/com/mttsui/security/model/mapper/SysRoleMapper.java
+++ security/src/main/java/com/mttsui/security/model/mapper/SysRoleMapper.java
@@ -3,8 +3,13 @@ package com.mttsui.security.model.mapper;
 import com.mttsui.security.model.entity.SysRole;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface SysRoleMapper {
+
+    List<SysRole> list();
+
     int deleteByPrimaryKey(String id);
 
     int insert(SysRole record);
