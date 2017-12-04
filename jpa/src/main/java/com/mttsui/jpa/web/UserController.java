1be6896ac75feccc8a6e964b6b24577df5b2a7f0
mttsui <xzs603@gmail.com>
2017-12-04 21:54:44 +0800

    Merge branch 'master' of https://github.com/xzs603/springboot-demos
    # Conflicts:
    #	security/src/main/java/com/mttsui/security/config/WebSecurityConfig.java
    #	security/src/main/java/com/mttsui/security/controller/UserController.java
    #	security/src/main/resources/application.yml
    #	security/src/main/resources/templates/user/crud.html

--- jpa/src/main/java/com/mttsui/jpa/web/UserController.java
+++ jpa/src/main/java/com/mttsui/jpa/web/UserController.java
@@ -1,4 +1,32 @@
 package com.mttsui.jpa.web;
 
+import com.mttsui.jpa.model.SysUserRepository;
+import com.mttsui.jpa.model.entity.SysUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private SysUserRepository sysUserRepository;
+
+    @RequestMapping("/list")
+    public List<SysUser> list(){
+        return sysUserRepository.findAll();
+    }
+
+    @RequestMapping("/findByName")
+    public SysUser findByName(SysUser sysUser){
+        return sysUserRepository.findByName(sysUser.getName());
+    }
+
+    @RequestMapping("/create")
+    public SysUser create(SysUser sysUser){
+        return sysUserRepository.save(sysUser);
+    }
 }
--- springboot-helloworld/src/main/java/com/mttsui/sbhelloworld/temp/HttpClientDemo.java
+++ springboot-helloworld/src/main/java/com/mttsui/sbhelloworld/temp/HttpClientDemo.java
@@ -1,4 +1,56 @@
 package com.mttsui.sbhelloworld.temp;
 
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
 public class HttpClientDemo {
+    private String server = "https://m.baidu.com";
+    private RestTemplate rest;
+    private HttpHeaders headers;
+    private HttpStatus status;
+
+    public static void main(String[] args) {
+        System.out.println(new HttpClientDemo().get("/"));
+    }
+
+    public HttpClientDemo() {
+        this.rest = new RestTemplate();
+        this.headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Accept", "*/*");
+    }
+
+    public String get(String uri) {
+        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
+        ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.GET, requestEntity, String.class);
+        this.setStatus(responseEntity.getStatusCode());
+        return responseEntity.getBody();
+    }
+
+    public String post(String uri, String json) {
+        HttpEntity<String> requestEntity = new HttpEntity<String>(json, headers);
+        ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.POST, requestEntity, String.class);
+        this.setStatus(responseEntity.getStatusCode());
+        return responseEntity.getBody();
+    }
+
+    public void put(String uri, String json) {
+        HttpEntity<String> requestEntity = new HttpEntity<String>(json, headers);
+        ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.PUT, requestEntity, (Class<String>) null);
+        this.setStatus(responseEntity.getStatusCode());
+    }
+
+    public void delete(String uri) {
+        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
+        ResponseEntity<String> responseEntity = rest.exchange(server + uri, HttpMethod.DELETE, requestEntity, (Class<String>) null);
+        this.setStatus(responseEntity.getStatusCode());
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
 }
