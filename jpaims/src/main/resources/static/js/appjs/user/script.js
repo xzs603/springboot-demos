e105e12dcc0cb3c409a35fe8c60867816cb6e5ec
mttsui <xzs603@gmail.com>
2018-01-05 13:34:43 +0800

    引入easyui布局

--- jpaims/src/main/resources/static/js/appjs/user/script.js
+++ jpaims/src/main/resources/static/js/appjs/user/script.js
@@ -1,1 +1,54 @@
-console.log('user load.')
+console.log('user load.');
+var url;
+function newUser(){
+    $('#dlg').dialog('open').dialog('setTitle','New User');
+    $('#fm').form('clear');
+    url = '/user/create';
+}
+function editUser(){
+    var row = $('#dg').datagrid('getSelected');
+    if (row){
+        $('#dlg').dialog('open').dialog('setTitle','Edit User');
+        $('#fm').form('load',row);
+        url = '/user/update';
+    }
+}
+function saveUser(){
+    $('#fm').form('submit',{
+        url: url,
+        onSubmit: function(){
+            return $(this).form('validate');
+        },
+        success: function(result){
+            var result = eval('('+result+')');
+            if (result.errorMsg){
+                $.messager.show({
+                    title: 'Error',
+                    msg: result.errorMsg
+                });
+            } else {
+                $('#dlg').dialog('close');		// close the dialog
+                $('#dg').datagrid('reload');	// reload the user data
+            }
+        }
+    });
+}
+function destroyUser(){
+    var row = $('#dg').datagrid('getSelected');
+    if (row){
+        $.messager.confirm('Confirm','Are you sure you want to destroy this user?',function(r){
+            if (r){
+                $.post('/user/delete',{id:row.id},function(result){
+                    if (result){
+                        $('#dg').datagrid('reload');	// reload the user data
+                    } else {
+                        $.messager.show({	// show error message
+                            title: 'Error',
+                            msg: result.errorMsg
+                        });
+                    }
+                },'json');
+            }
+        });
+    }
+}
