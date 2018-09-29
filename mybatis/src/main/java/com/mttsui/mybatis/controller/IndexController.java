cd6d452c36df6077911f556c03356f0edc271322
mttsui <xzs603@gmail.com>
2018-09-29 15:20:56 +0800

    新增文件下载功能

--- mybatis/src/main/java/com/mttsui/mybatis/controller/IndexController.java
+++ mybatis/src/main/java/com/mttsui/mybatis/controller/IndexController.java
@@ -1,8 +1,17 @@
 package com.mttsui.mybatis.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.URLEncoder;
 
 @RequestMapping("/index")
 @RestController
@@ -12,4 +21,25 @@ public class IndexController {
     public String index() {
         return "This is the index page.";
     }
+
+    @ApiOperation("下载模板文件")
+    @GetMapping("download")
+    ResponseEntity<InputStreamResource> downloadFile(String log)
+            throws IOException {
+        String filePath = "d:/" + "血压管理-领导报表-springcloud配置.txt";
+        FileSystemResource file = new FileSystemResource(filePath);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", URLEncoder.encode(file.getFilename(), "utf-8")));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(file.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new InputStreamResource(file.getInputStream()));
+    }
+
 }
