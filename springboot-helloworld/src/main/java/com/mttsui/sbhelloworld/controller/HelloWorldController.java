fab5f4b503925b6610772112c22f4aea6310e0e6
zsxu2 <hahaha1!>
2019-10-17 10:21:39 +0800

    测试Host重写

--- springboot-helloworld/src/main/java/com/mttsui/sbhelloworld/controller/HelloWorldController.java
+++ springboot-helloworld/src/main/java/com/mttsui/sbhelloworld/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.mttsui.sbhelloworld.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Administrator on 2017/11/20 0020.
@@ -12,5 +15,11 @@ public class HelloWorldController {
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+    @GetMapping("/host")
+    public String index(HttpServletRequest request){
+        String host = request.getHeader("Host");
+        return "Host: " + host;
     }
 }
